333e77675e4ccca3724f09d0722fd2b316186a68
Sofía Zorrilla <sofia.zorrilla95@gmail.com>
2024-01-17 16:47:27 -0600

    Refinamiento del script para unir registros
    Añadi código para poder revisar qué tan lejos del rango de elevación reportado están los registros..
    
    Además añadí código para extraer la elevación a partir de las coordenadas de los registros de literatura.

--- bin/union_biblio_gbif.R
+++ bin/union_biblio_gbif.R
@@ -1,24 +1,66 @@
-## Unir registros bibliográficos con los de gbif 
+
+
 
 library(tidyverse)
 library(googlesheets4)
+library('elevatr')
+library(sf)
 
-## Funciones 
+##################
+### Funciones ###
+##################
 
-# Function to check if any word in ColumnA (df1) is present in ColumnB (df2)
+
+## check_elev
+### Funcion para revisar si un registro está dentro del rango de elevación reportado para la especie
+## Datos de entrada: 
+
+# Tabla de taxonomía con rangos de elevación reportados y nombres de las especies.
+# Tabla con registros de elevación y nombre de la especie
+# Ambas tablas deben tener una columna de ID_SP con la que se puedan unir 
 
 
+check_elev <- function(elev, maxLim, minLim, sd){
+  ifelse(elev >= minLim & elev <= maxLim, 
+         return(1),
+         ifelse(elev >= (minLim-sd) & elev <= (maxLim+sd), return(2),
+                return(0)))
+}
+
+
+
+
+
+## Link a hoja de datos
 ss <- "https://docs.google.com/spreadsheets/d/12eHA_LwHQ2m2EG441GP8XkquLYURjJfHkVZ2h699ZGM/edit?usp=sharing"
+
+## Leer tablas de datos
 
 gbif <- read_sheet(ss = ss, sheet = "registros_gbif", col_types = "c") %>% filter(basisOfRecord == "PRESERVED_SPECIMEN")
 tax <- read_sheet(ss = ss, sheet = "tax_gbif", col_types = "c") %>% filter(status == "ACCEPTED") %>% arrange(ID_SP) 
 ref <- read_sheet(ss = ss, sheet = "referencias", col_types = "c")
 biblio <-  read_sheet(ss = ss, sheet = "registros_bibliografia", col_types = "c")
 
-join_gbif <- left_join(gbif,tax,by = "ID_SP") %>% 
-  left_join(ref,by = "ID_referencia") %>%
-  mutate(AUTORIDAD = paste(author,year.y), 
-         FECHA = paste(day,month,year.x, sep = "/"), 
+## Unir datos de GBIF con taxonomia y evaluar si están dentro del limite altitudinal reportado para la especie
+## inRange es un flag que indica 1 si está dentro del rango reportado, 2 si está dentro del rango +- una desviación estándar
+## calculada con los mismos datos de GBIF para Guanajuato y 0 cuando está a más de una desviación estandar. 
+
+join_gbif_tax <- left_join(gbif,tax,by = "ID_SP") %>% 
+  group_by(ID_SP) %>% 
+  mutate(elevation_y = as.numeric(elevation_y),
+         Limite_altitudinal_max = as.numeric(Limite_altitudinal_max),
+         Limite_altitudinal_min = as.numeric(Limite_altitudinal_min),
+         mean_elev = mean(elevation_y), 
+         sd_elev = sd(elevation_y)) %>% 
+  rowwise() %>% 
+  mutate(inRange = check_elev(elev = elevation_y, minLim = Limite_altitudinal_min, maxLim = Limite_altitudinal_max, sd = sd_elev)) %>% ungroup()
+
+
+## Unir datos de GBIF con referencias y seleccionar las columnas pertinentes para el reporte
+  
+join_gbif <- join_gbif_tax %>% left_join(ref,by = "ID_referencia") %>%
+  mutate(AUTORIDAD = paste(author,yearAuth), 
+         FECHA = paste(day,month,year, sep = "/"), 
          NOMBRE_COMUN = "Encino", 
          PHYLUM = "Tracheophyta",
          CLASE = "Magnoliopsida",
@@ -52,15 +94,52 @@ join_gbif <- left_join(gbif,tax,by = "ID_SP") %>%
 
 
 
+################################
+## Registros de bibliografia ###
+################################
+
+## Filtrar registros con coordenadas faltantes
+
+biblio <- filter(biblio, !is.na(LONGITUD))
 
 
+## Modificar columnas en la tabla de registros de bibliografia para poder unir con la taxonomía 
 
 join_biblo <- biblio %>% 
-  mutate(specificEpithet = str_remove(nombre,"Quercus ")) %>% 
   mutate(specificEpithet = ifelse(!is.na(sinonimia),sinonimia,specificEpithet), `TIPO DE REGISTRO` = "articulo") %>% 
-  left_join(tax,by = "specificEpithet") %>% 
-  left_join(ref,by = "ID_referencia") %>% 
-  mutate(AUTORIDAD = paste(author,year), 
+  mutate(LONGITUD = as.numeric(LONGITUD), LATITUD = as.numeric(LATITUD)) %>% 
+  left_join(tax,by = "specificEpithet")
+  
+## Añadir columna de elevación
+
+coords <- join_biblo[,c("LONGITUD","LATITUD")]
+crs <- 4326
+coords_sf <- st_as_sf(coords, coords = c("LONGITUD","LATITUD"), crs = crs)
+elev <- elevatr::get_elev_point(coords_sf, prj = crs, src = "epqs") |> as.data.frame()
+join_biblo$elevation <- elev$elevation
+
+
+## Revisar rangos de elevación
+
+join_biblo <- join_biblo %>% 
+  group_by(ID_SP) %>% 
+  mutate(elevation_y = as.numeric(elevation),
+         Limite_altitudinal_max = as.numeric(Limite_altitudinal_max),
+         Limite_altitudinal_min = as.numeric(Limite_altitudinal_min),
+         mean_elev = mean(elevation), 
+         sd_elev = sd(elevation)) %>% 
+  rowwise() %>% 
+  mutate(inRange = check_elev(elev = elevation, minLim = Limite_altitudinal_min, maxLim = Limite_altitudinal_max, sd = sd_elev))%>% ungroup()
+
+
+## Cuantos registros de cada categoria de la columna inRange hay
+table(join_biblo$inRange)
+
+
+## Seleccionar y añadir columnas pertinentes para el reporte 
+join_biblo <- join_biblo %>% 
+left_join(ref,by = "ID_referencia") %>% 
+  mutate(AUTORIDAD = paste(author,yearAuth), 
          NOMBRE_COMUN = "Encino",
          PHYLUM = "Tracheophyta",
          CLASE = "Magnoliopsida",
